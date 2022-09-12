c4ac4217674f6042a9815c02a665337ee440102e
R2D2-like <103891981+R2D2-like@users.noreply.github.com>
2022-09-12 16:09:39 +0900

    Create  task2_manager.py

---
+++ catkin_ws/src/project/scripts/task2_manager.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python3
+import numpy as np
+
+import rospy
+import tf
+from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+from std_msgs.msg import String
+from project.srv import WavingLeftRight
+from project.srv import GetCoordinate
+
+import tf2_ros
+import actionlib
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+from geometry_msgs.msg import Quaternion
+from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import Quaternion
+
+
+class SimpleController:
+    def __init__(self):
+        rospy.init_node('simple_controller', anonymous=True)
+        
+        # Publisher
+        self.cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+
+        # Subscriber
+        odom_sub = rospy.Subscriber('/odom', Odometry, self.callback_odom)
+
+        self.x = None
+        self.y = None
+        self.yaw = None
+        while self.x is None:
+            rospy.sleep(0.1)
+
+    def callback_odom(self, data):
+        self.x = data.pose.pose.position.x
+        self.y = data.pose.pose.position.y
+        self.yaw = self.get_yaw_from_quaternion(data.pose.pose.orientation)
+
+    def go_straight(self, dis, velocity=0.3):
+        vel = Twist()
+        x0 = self.x
+        y0 = self.y
+        while(np.sqrt((self.x-x0)**2+(self.y-y0)**2)<dis):
+            vel.linear.x = velocity
+            vel.angular.z = 0.0
+            self.cmd_vel_pub.publish(vel)
+            rospy.sleep(0.1)
+        self.stop()
+
+    def turn_right(self, yaw, yawrate=-0.5):
+        vel = Twist()
+        yaw0 = self.yaw
+        while(abs(self.yaw-yaw0)<np.deg2rad(yaw)):
+            vel.linear.x = 0.0
+            vel.angular.z = yawrate
+            self.cmd_vel_pub.publish(vel)
+            rospy.sleep(0.1)
+        self.stop()
+
+    def turn_left(self, yaw, yawrate=0.5):
+        vel = Twist()
+        yaw0 = self.yaw
+        while(abs(self.yaw-yaw0)<np.deg2rad(yaw)):
+            vel.linear.x = 0.0
+            vel.angular.z = yawrate
+            self.cmd_vel_pub.publish(vel)
+            rospy.sleep(0.1)
+        self.stop()
+
+    def stop(self):
+        vel = Twist()
+        vel.linear.x = 0.0
+        vel.angular.z = 0.0
+        self.cmd_vel_pub.publish(vel)
+
+    def get_yaw_from_quaternion(self, quaternion):
+        e = tf.transformations.euler_from_quaternion(
+                (quaternion.x, quaternion.y, quaternion.z, quaternion.w))
+        return e[2]
+
+
+
+class ActionGoal:
+    def __init__(self):
+        #self.ps_pub = rospy.Publisher('move_base_simple/goal', PoseStamped, queue_size=1)
+        self.action_client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+        self.action_client.wait_for_server()  # action serverの準備ができるまで待つ
+
+    def set_goal(self, x, y, yaw):
+        self.goal = MoveBaseGoal()  # goalのメッセージの定義
+        self.goal.target_pose.header.frame_id = 'map'  # マップ座標系でのゴールとして設定
+        self.goal.target_pose.header.stamp = rospy.Time.now()  # 現在時刻
+        
+        # ゴールの姿勢を指定
+        self.goal.target_pose.pose.position.x = x
+        self.goal.target_pose.pose.position.y = y
+        q = tf.transformations.quaternion_from_euler(0.0, 0.0, yaw)  # 回転はquartanionで記述するので変換
+        self.goal.target_pose.pose.orientation = Quaternion(q[0], q[1], q[2], q[3])
+ 
+    # def send_topic(self):
+    #     #self.ps_pub.publish(self.goal)
+    def send_action(self, duration=30.0):
+        self.action_client.send_goal(self.goal)  # ゴールを命令
+        result = self.action_client.wait_for_result(rospy.Duration(duration))
+        return result
+
+
+if __name__=='__main__':
+
+    #go to wave detection point (step1)
+    simple_controller = SimpleController()
+    try:
+        simple_controller.go_straight(1.0)
+        simple_controller.turn_left(90)
+    except rospy.ROSInitException:
+        pass
+
+    #reqest waving person result(step2)
+    waving_person = rospy.ServiceProxy('/wave_detection', WavingLeftRight)
+    res  = waving_person()
+
+    #induce masking ankle(step3)
+    mask_ankle_trigger_pub = rospy.Publisher('/mask/ankle/trigger', String)
+    waving_person_str = String()
+    waving_person_str.data = res.left_or_right
+    mask_ankle_trigger_pub.publish(waving_person_str)
+    rospy.sleep(0.05)
+
+
+    #reqest goal coordinate(step4)
+    get_coordinate = rospy.ServiceProxy('/get_coordinate', GetCoordinate)
+    res = get_coordinate()
+    x = res.x
+    y = res.y
+
+    #send goal (step5)
+    rate = rospy.Rate(10)
+    ac = ActionGoal()
+    ac.set_goal(x, y, 0.0)
+    res = ac.send_action()
+    print(res)
+    rate.sleep()
+
+
+
+    
+
+     
+
+
